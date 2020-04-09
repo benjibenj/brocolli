d8a67089aaaf87ce7469095f284e8920d4dce189
Benjamin Vaysse <benjamin.vaysse@gmail.com>
2020-04-08 19:57:21 -0500

    add rules/color.js

---
+++ src/rules/colors.js
@@ -0,0 +1,50 @@
+// Primary colors
+const orange = {
+  default: "#f45d48",
+  lighter: "#ED992F",
+  darker: "#E6592F",
+};
+
+const eggShell = {
+  default: "#f8f5f2",
+};
+
+const blue = {
+  default: "#078080",
+}
+
+// Utility colors
+const white = {
+  default: "#f8f5f2",
+};
+const black = {
+  default: "#333132",
+};
+
+const headline = {
+  default: "#232323",
+};
+const subHeadline = {
+  default: "#222525"
+}
+const paragraph = {
+  default: "#222525",
+};
+
+
+const Colors = {
+  primary: {
+    orange,
+    blue,
+    eggShell
+  },
+  utility: {
+    headline,
+    subHeadline,
+    paragraph,
+    white,
+    black,
+  }
+};
+
+export default Colors;
